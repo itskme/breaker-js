3f95be7670029475bb158acec27054020f6487a3
itskme <160423464+itskme@users.noreply.github.com>
2024-07-27 18:54:19 +0000

    Finish the core game

--- breakout.js
+++ breakout.js
@@ -19,7 +19,6 @@ let ballRadius = 10;
 let bricks = [];
 let score = 0;
 
-
 for (let i = 0; i < 5; i++) {
   for (let j = 0; j < 10; j++) {
     bricks.push({
@@ -32,3 +31,87 @@ for (let i = 0; i < 5; i++) {
   }
 }
 
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'black';
+  ctx.fillRect(paddleX, paddleY, paddleWidth, paddleHeight);
+  
+
+  ctx.beginPath();
+  ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
+  ctx.fillStyle = 'black'; 
+  ctx.fill();
+  
+  for (let brick of bricks) {
+    if (brick.visible) {
+      ctx.fillStyle = 'red';
+      ctx.fillRect(brick.x, brick.y, brick.width, brick.height);
+    }
+  }
+  ctx.font = '24px Arial';
+  ctx.fillStyle = 'black';
+  ctx.textAlign = 'left';
+  ctx.textBaseline = 'top';
+  ctx.fillText(`Score: ${score}`, 10, 10);
+}
+
+
+function update() {
+
+  if (keysPressed['ArrowLeft'] && paddleX > 0) {
+    paddleX -= 5;
+  }
+  if (keysPressed['ArrowRight'] && paddleX < canvas.width - paddleWidth) {
+    paddleX += 5;
+  }
+
+ 
+  ballX += ballSpeedX;
+  ballY += ballSpeedY;
+
+  
+  if (ballY + ballRadius > paddleY && ballX > paddleX && ballX < paddleX + paddleWidth) {
+    ballSpeedY = -ballSpeedY;
+  }
+
+ 
+  for (let brick of bricks) {
+    if (brick.visible && ballX > brick.x && ballX < brick.x + brick.width && ballY > brick.y && ballY < brick.y + brick.height) {
+      brick.visible = false;
+      ballSpeedY = -ballSpeedY;
+      score++;
+    }
+  }
+
+ 
+  if (ballX + ballRadius > canvas.width || ballX - ballRadius < 0) {
+    ballSpeedX = -ballSpeedX;
+  }
+  if (ballY - ballRadius < 0) {
+    ballSpeedY = -ballSpeedY;
+  }
+
+  if (ballY + ballRadius > canvas.height) {
+    alert('Game Over!');
+    location.reload();
+  }
+}
+
+
+const keysPressed = {};
+document.addEventListener('keydown', (e) => {
+  keysPressed[e.key] = true;
+});
+document.addEventListener('keyup', (e) => {
+  keysPressed[e.key] = false;
+});
+
+
+function gameLoop() {
+  update();
+  draw();
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
